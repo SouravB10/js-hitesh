4ed8f7c7ab06cdd840f68dbae17d3906ce13e296
Sourav Behuria <163456340+SouravB10@users.noreply.github.com>
2025-03-18 07:16:46 +0000

    conversions

--- 01basics/02datatypes.js
+++ 01basics/02datatypes.js
@@ -15,7 +15,7 @@ console.log(typeof null);
 // string = "assembly of characters"
 // boolean = true or false
 // null = stand alone value (type = object)
-// undefined = kindda place holder for a value which is not defined yet (type = undefined)
+// undefined = kinda place holder for a value which is not defined yet (type = undefined)
 // symbol = uniquness
 
 
---
+++ 01basics/03conversions.js
@@ -0,0 +1,25 @@
+let a = 233
+
+//console.log(typeof a);
+
+let strnum = String(a);
+
+//console.log(strnum);
+//console.log(typeof strnum);
+
+let strTest = 'saasas';
+let bool = Boolean(strTest);
+
+// console.log(bool);
+// console.log(typeof bool);
+
+// console.log(2**4);
+
+let f=2;
+// let t=++f;
+let h=f++
+
+console.log(`original value:${f}, ${h}`);
+
+
+
